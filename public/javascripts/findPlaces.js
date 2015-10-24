d7d4e04bd1a9f36420896c0a657ed2c8424b647f
Derek Benson <derek.benson@tufts.edu>
2015-10-24 06:30:28 +0000

    refactored plotRoute to setInterval

--- public/javascripts/findPlaces.js
+++ public/javascripts/findPlaces.js
@@ -1,10 +1,10 @@
 var API = '4F5A3954F085445486E190A252ED8DB9';
 var website = 'https://api.tripadvisor.com/api/partner/2.0/';
 
-
 function findPlaces(lat, lon) {
     //find places near location
     var locations = [];
+    var review = [];
     var placesAPI = website + 'map/' + lat + ',' + lon + '?key=' + API;
     $.getJSON(placesAPI, function(text) {
         for (var i = 0; i < text.data.length; i++) {
@@ -12,26 +12,25 @@ function findPlaces(lat, lon) {
             locations[i].lat = text.data[i].latitude;
             locations[i].lng = text.data[i].longitude;
             locations[i].locId = text.data[i].location_id;
-            console.log(locations[i].lat);
-            var review = getReviews(locations[i].locId);
-            locations[i].title = review.title;
-            locations[i].text = review.text;
-            locations[i].rating = review.rating;
+            locations[i].type = text.data[i].category;
+            review[i] = getReviews(locations[i].locId);
+            console.log(getReviews(locations[i].locId));
+            //locations[i].title = review[i].title;
         }
     });
-    return locations;
+    return [locations, review];
 }
 
 function getReviews(location) {
     //get reviews for given place
     var reviewsAPI = website + 'location/' + location + '/reviews?key=' + API;
     $.getJSON(reviewsAPI, function(text) {
-        var review = text.data[Math.round(Math.random()*text.num_reviews)];
+        var review = text.data[0];//Math.round(Math.random()*1)];
         var title = review.title;
         var text = review.text;
         var rating = review.rating;
         var reviewData = [title, text, rating];
-        console.log(reviewData);
+        //console.log(reviewData);
         return reviewData;
     });
 }
--- public/javascripts/plotRoute.js
+++ public/javascripts/plotRoute.js
@@ -10,66 +10,106 @@ var skeletonMarker;
 
 var skeletonRoute = null;
 
-var image = 'images/small_skull.png';
-//var image = '../public/images/skeleton_anim_angry.gif';
+var image = 'images/skeleton_anim_angry.gif';
 
-function plotRoute(locations){
-	for (var n = 0; n < locations.length; n ++) {
+function plotRoute(locations) {
+	for (var n = 0; n < locations.length; n++) {
 		locations[n].lat = parseFloat(locations[n].lat);
 		locations[n].lng = parseFloat(locations[n].lng);
 	}
-	map = new google.maps.Map(document.getElementById("map"), 
-						{center: locations[0],
-						zoom: 8});
+	map = new google.maps.Map(document.getElementById("map"), {
+		center: locations[0],
+		zoom: 8
+	});
 	var directionsService = new google.maps.DirectionsService;
-  	var directionsDisplay = new google.maps.DirectionsRenderer;
+	var directionsDisplay = new google.maps.DirectionsRenderer;
 	directionsDisplay.setMap(map);
-	
+
 	var waypointSize = locations.length;
-	
+
 	if (locations.length >= 9) {
-		waypointSize = 8;	
+		waypointSize = 8;
 	}
 	var waypoints = new Array(waypointSize);
-	
-	for(var i = 0; i < waypoints.length && i < 8; i++){
-		waypoints[i] = {location:locations[i+1], stopover:true};
+
+	for (var i = 0; i < waypoints.length && i < 8; i++) {
+		waypoints[i] = {
+			location: locations[i + 1],
+			stopover: true
+		};
 	}
 
 	calcRoute(locations[0], waypoints, map, directionsService, directionsDisplay);
 }
 
 function calcRoute(start, stops, map, directionsService, directionsDisplay) {
-  	var request = {
-    	origin:start,
-    	destination:start,
+	var request = {
+		origin: start,
+		destination: start,
 		travelMode: google.maps.TravelMode.WALKING,
 		waypoints: stops,
 		optimizeWaypoints: true
 	};
-	
-	
+
+
 	directionsService.route(request, function(result, status) {
-    	if (status == google.maps.DirectionsStatus.OK) {
-    		skeletonRoute = result;
-      		directionsDisplay.setDirections(result);
-      		skeletonMarker = new google.maps.Marker({position:start, map:map, icon:image});
-      		animateSkelly(skeletonMarker, skeletonRoute, 0, 0, 0, 0, 0)
-    	}
+		if (status == google.maps.DirectionsStatus.OK) {
+			skeletonRoute = result;
+			directionsDisplay.setDirections(result);
+			skeletonMarker = new google.maps.Marker({
+				position: start,
+				map: map,
+				icon: image
+			});
+
+			var route = 0;
+			var leg = 0;
+			var step = 0;
+			var latlng = 0;
+
+			function animateSkelly() {
+
+				//var prevLoc = skeletonRoute.routes[route].legs[leg].steps[step].path[latlng];
+				latlng++;
+				if (latlng >= skeletonRoute.routes[route].legs[leg].steps[step].path.length) {
+					latlng = 0;
+					step++;
+				}
+				if (step >= skeletonRoute.routes[route].legs[leg].steps.length) {
+					step = 0;
+					leg++;
+				}
+				if (leg >= skeletonRoute.routes[route].legs.length) {
+					leg = 0;
+					route++;
+				}
+				if (route >= skeletonRoute.routes.length) {
+					route = 0;
+				}
+				//var nextLoc = ;
+
+				skeletonMarker.setPosition(skeletonRoute.routes[route].legs[leg].steps[step].path[latlng]);
+			}
+			window.setInterval(animateSkelly, 500);
+		}
 	});
 }
 
 //to get discrete points from route, use route[0].legs[0].steps[0].path[0];
 
 
-function plopMarkers(){
+function plopMarkers() {
 	console.log(skeletonRoute);
-	
-	for(var route = 0; route < skeletonRoute.routes.length; route++){
-		for(var leg = 0; leg < skeletonRoute.routes[route].legs.length; leg++){
-			for(var step = 0; step < skeletonRoute.routes[route].legs[leg].steps.length; step++){
-				for(var latlng = 0; latlng < skeletonRoute.routes[route].legs[leg].steps[step].path.length; latlng++){
-					new google.maps.Marker({position:skeletonRoute.routes[route].legs[leg].steps[step].path[latlng], map:map, icon:image});
+
+	for (var route = 0; route < skeletonRoute.routes.length; route++) {
+		for (var leg = 0; leg < skeletonRoute.routes[route].legs.length; leg++) {
+			for (var step = 0; step < skeletonRoute.routes[route].legs[leg].steps.length; step++) {
+				for (var latlng = 0; latlng < skeletonRoute.routes[route].legs[leg].steps[step].path.length; latlng++) {
+					new google.maps.Marker({
+						position: skeletonRoute.routes[route].legs[leg].steps[step].path[latlng],
+						map: map,
+						icon: image
+					});
 					//skeletonMarker({position:skeletonRoute.routes[route].legs[leg].steps[step].path[latlng]});
 				}
 			}
@@ -78,34 +118,11 @@ function plopMarkers(){
 
 }
 
-function animateSkelly(yourmarker,yourroute,route,leg,step,latlng, step) {
-	var prevLoc = yourroute.routes[route].legs[leg].steps[step].path[latlng];
-	latlng++;
-	if (latlng >= yourroute.routes[route].legs[leg].steps[step].path.length){
-		latlng = 0;
-		step++;
-	}
-	if (step >= yourroute.routes[route].legs[leg].steps.length){
-		step = 0;
-		leg++;
-	}
-	if (leg >= yourroute.routes[route].legs.length){
-		leg = 0;
-		route++;
-	}
-	if (route >= yourroute.routes.length){
-		route = 0;
-	}
-	var nextLoc = yourroute.routes[route].legs[leg].steps[step].path[latlng];
-	
-	skeletonMarker.setPosition(lerp(prevLoc, nextLoc, step));
-    window.setTimeout(function(){
-        animateSkelly(yourmarker,yourroute,route,leg,step,latlng, (step+0.1)%1);
-    },500);
-}
 
-function lerp(latlngA, latlngB, n){
-	var lat = (n*latlngB.lat+(1-n)*latlngA.lat)/2;
-	var lng = (n*latlngB.lng+(1-n)*latlngA.lng)/2;
-	return {lat, lng};
+function lerp(latlngA, latlngB, n) {
+	var lat = (n * latlngB.lat + (1 - n) * latlngA.lat) / 2;
+	var lng = (n * latlngB.lng + (1 - n) * latlngA.lng) / 2;
+	return {
+		lat, lng
+	};
 }
